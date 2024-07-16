6a1b052255635c87cc4f66090fb60d7d3cef6b1d
emin1m <easky20@hotmail.com>
2024-07-16 09:46:01 +0300

    add Bug_Fixed

--- cli.py
+++ cli.py
@@ -1,23 +1,38 @@
-# Command Line Interface, kodlar CLI üzerinden etkili bir şekilde çalıştırılabilir.
-
 import argparse
 
-from predictor import DepthEstimationModel
+def initialize_model(): # we have defined this as a function otherwise, this code starts automatically
+    # and can  not import other functions such as --help or --print-settings
+    from predictor import DepthEstimationModel
+    return DepthEstimationModel()
 
 def main():
     parser = argparse.ArgumentParser(description="Depth estimation using ZoeDepth.")
-    parser.add_argument("input_image",help = "Path to input image.")
-    parser.add_argument("output_image",help = "Path to output depth map.")
+    parser.add_argument("input_image", nargs='?', help="Path to input image.")
+    parser.add_argument("output_image", nargs='?', help="Path to output depth map.")
+    parser.add_argument('--print-settings', action='store_true', help='Print the default settings and exit.')
+
     args = parser.parse_args()
 
-    model = DepthEstimationModel()
+    if args.print_settings:
+        print_settings()
+        return
 
-    result = model.calculate_depthmap(args.input_image, args.output_image)
+    if args.input_image and args.output_image:
+        model = initialize_model()
+        result = model.calculate_depthmap(args.input_image, args.output_image)
+        print(result)
+    else:
+        parser.print_help()
 
-    print(result)
+def print_settings():
+    settings = {
+        "model_name": "DepthEstimationModel",
+        "input_format": "PNG, JPG",
+        "output_format": "PNG",
+    }
+    print("Default Settings:")
+    for key, value in settings.items():
+        print(f"{key}: {value}")
 
-if __name__ == "__main__": # başka bir python scripti tarafından cli file'in otomatik olarak import edilmesini engellemek için yazılır.
+if __name__ == "__main__":
     main()
-
-## cli.py sayesinde artık predictor dışarıdan terminal sayesinde çalıştırılabilr
-# örnek kod python cli.py test_image.png output_cli.png
